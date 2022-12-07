9ee4382088ca3fc049989dbcfaa66cdfd27c983e
James Hueston <jameshueston@gmail.com>
2022-12-07 17:38:16 -0500

    getEmployeesByTaskName byRows remarshalled to JSON working static input

--- main.go
+++ main.go
@@ -125,7 +125,6 @@ func getEmployeesByTaskName(w http.ResponseWriter, r *http.Request) {
 	//var employees []Employee
 	//var task Task
 	//var employeeTasks []EmployeeTask
-	//var employeeEmailTaskNames []EmployeeEmailTaskName
 
 	// "searchterm" accepts partial name matches, case-insensitive
 	// db.First(&task, params["searchterm"])
@@ -134,70 +133,62 @@ func getEmployeesByTaskName(w http.ResponseWriter, r *http.Request) {
 	//whereConditionFormatted := fmt.Sprintf("LOWER(Tasks.Name) LIKE LOWER('\%%s\%')", params["searchterm"])
 	db.LogMode(true)
 
-	var results []map[string]interface{}
+	//var results []map[string]interface{}
 	//db.Table("users").Find(&results)
-	var EmployeeEmail string
-	var TaskName string
 
 	//db.Raw("SELECT Employees.email AS EmployeeEmail, Tasks.Name AS TaskName FROM ((EmployeeTask INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID) INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID) WHERE LOWER(Tasks.Name) LIKE LOWER('%new%')").Scan(&results)
+	// Finally decided to execute raw
+	// Created a new struct with only EmployeeEmail and TaskName but Gorm wouldn't write to it
+	// Resorted to the old manual extrapoloation
+	var employeeEmail string
+	var taskName string
+	var employeeEmailTaskNames []EmployeeEmailTaskName
+
 	rows, err := db.Raw("SELECT Employees.email AS EmployeeEmail, Tasks.Name AS TaskName FROM ((EmployeeTask INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID) INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID) WHERE LOWER(Tasks.Name) LIKE LOWER('%new%')").Rows()
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
-		rows.Scan(&EmployeeEmail, &TaskName)
-		fmt.Println("EmployeeEmail:" + EmployeeEmail + " TaskName: " + TaskName)
+		rows.Scan(&employeeEmail, &taskName)
+		// The following debug prints, so saving that, remarshalling
+		fmt.Println("EmployeeEmail:" + employeeEmail + " TaskName: " + taskName)
+		result := EmployeeEmailTaskName{EmployeeEmail: employeeEmail, TaskName: taskName}
+		employeeEmailTaskNames = append(employeeEmailTaskNames, result)
 	}
 
 	/*
+		// First I wrote raw SQL that WORKS
+		SELECT Employees.email AS EmployeeEmail, Tasks.Name AS TaskName
+		FROM ((EmployeeTask
+		INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID)
+		INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID)
+		WHERE LOWER(Tasks.Name) LIKE LOWER('%new%');
+
+		// Next, I tried but failed several Gorm idiomatic Joins: https://gorm.io/docs/query.html#Joins
+		db.Model(&EmployeeTask{}).Select(
+		"Employees.email, Tasks.name").Joins(
+		"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
+		"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
+		"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Scan(&employees)
+
+		db.Select("Employees.email, Tasks.name").Joins(
+		"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
+		"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
+		"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Find(&results)
+
 		db.Table("employeetask").Select("Employees.email, Tasks.name").Joins(
-			"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
-			"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
-			"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Scan(&results)
+		"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
+		"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
+		"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Scan(&results)
 
-		/*
-			db.Select("Employees.email, Tasks.name").Joins(
-			"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
-			"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
-			"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Find(&results)
-
-			db.Model(&EmployeeTask{}).Select(
-			"Employees.email, Tasks.name").Joins(
-			"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
-			"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
-			"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Scan(&employees)
-
-			// WORKS - Returning Employee Fields but not yet Task Name field
-			db.Table("employeetask").Select("Employees.ID, Employees.email, Employees.Phone, Employees.Role").Joins(
-			"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
-			"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
-			"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Scan(&employees)
-
-			// WORKS - Search Task name by keyword, ignoring case
-			SELECT Employees.email AS EmployeeEmail, Tasks.Name AS TaskName
-			FROM ((EmployeeTask
-			INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID)
-			INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID)
-			WHERE LOWER(Tasks.Name) LIKE LOWER('%new%');
-
-
-
-
-				db.Model(&User{}).Select("users.name, emails.email").Joins("left join emails on emails.user_id = users.id").Scan(&result{})
-				// SELECT users.name, emails.email FROM `users` left join emails on emails.user_id = users.id
-
-				rows, err := db.Table("users").Select("users.name, emails.email").Joins("left join emails on emails.user_id = users.id").Rows()
-				for rows.Next() {
-					...
-				}
-
-				db.Table("users").Select("users.name, emails.email").Joins("left join emails on emails.user_id = users.id").Scan(&results)
-
-				// multiple joins with parameter
-				db.Joins("JOIN emails ON emails.user_id = users.id AND emails.email = ?", "jinzhu@example.org").Joins("JOIN credit_cards ON credit_cards.user_id = users.id").Where("credit_cards.number = ?", "411111111111").Find(&user)
-
+		// WORKS to return Employee Fields but I wanted Task Name, too
+		db.Table("employeetask").Select("Employees.ID, Employees.email, Employees.Phone, Employees.Role").Joins(
+		"INNER JOIN Employees ON EmployeeTask.EmployeeID = Employees.ID").Joins(
+		"INNER JOIN Tasks ON EmployeeTask.TaskID = Tasks.ID").Where(
+		"LOWER(Tasks.Name) LIKE LOWER('%" + params["searchterm"] + "%')").Scan(&employees)
 	*/
-	fmt.Println(&results)
-	json.NewEncoder(w).Encode(&results)
+
+	fmt.Println(&employeeEmailTaskNames)
+	json.NewEncoder(w).Encode(&employeeEmailTaskNames)
 }
