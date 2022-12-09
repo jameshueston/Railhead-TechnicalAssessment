6e9f65c5f248812b7dd7f896b45f1ce2aa2d8331
James Hueston <jameshueston@gmail.com>
2022-12-09 14:49:39 -0500

    load workers db with data

--- db/init-workers-db.sh
+++ db/init-workers-db.sh
@@ -5,3 +5,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE DATABASE workers;
 	GRANT ALL PRIVILEGES ON DATABASE workers TO postgres;
 EOSQL
+
+# Load the 'workers' database with tables, constraints and seed records
+# Note: 'initsql' is NOT 'init.sql' to prevent auto-running on default 'postgres' db
+# https://github.com/docker-library/docs/tree/master/postgres#initialization-scripts
+psql --username "$POSTGRES_USER" --dbname=workers --file=/docker-entrypoint-initdb.d/initsql
